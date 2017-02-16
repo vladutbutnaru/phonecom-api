c4baaad33a9b37e89e0fe5690a98ea6acabd0f8c
vladutbutnaru <vlad.butnaru@feel-it-services.com>
2017-02-16 11:50:33 +0200

    Added list and get, changed Switch, added flags

--- src/phonecom/phonecom.go
+++ src/phonecom/phonecom.go
@@ -17,25 +17,201 @@ func main() {
       Value: "list-accounts",
         Usage: "API command that you want to execute",
     },
+       cli.IntFlag{
+      Name: "id",
+      Value: 0,
+        Usage: "ID of entity you want to operate",
+    },
+        cli.IntFlag{
+      Name: "limit",
+      Value: 5,
+        Usage: "Upper limit of results you want to fetch",
+    },
+        cli.IntFlag{
+      Name: "offset",
+      Value: 0,
+        Usage: "Offset of results you want to fetch",
+    },
   }
 
   app.Action = func(c *cli.Context) error {
    
-  
-    if c.String("command") == "list-extensions" {
-        var extensionsApi swagger.ExtensionsApi = *swagger.NewExtensionsApi()
+      switch c.String("command") {
+          case "list-media":
+     
+     
+       
+        var mediaApi swagger.MediaApi = *swagger.NewMediaApi()
         mySlice1 := make([]string, 0)
-        x,response,error := extensionsApi.ListAccountExtensions(1315091, mySlice1, mySlice1, mySlice1, "", "", "", 3, 1, "")
-        fmt.Printf("%+v\n",x)
-        fmt.Println("")
-        fmt.Println("Response:")
-        fmt.Printf("%+v\n", response)
-        fmt.Println(error)
+        x,response,error := mediaApi.ListAccountMedia(1315091, mySlice1, mySlice1, "", "", int32( c.Int("limit")), int32( c.Int("offset")), "")
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+       
         
-      
-    } else {
-      fmt.Println("Command not valid")
-    }
+          case "get-recording":
+           var mediaApi swagger.MediaApi = *swagger.NewMediaApi()
+          x,response,error := mediaApi.GetAccountMedia(1315091, int32(c.Int("id")))
+          if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+          case "list-menus":
+           var menusApi swagger.MenusApi = *swagger.NewMenusApi()
+           mySlice1 := make([]string, 0)
+        x,response,error := menusApi.ListAccountMenus(1315091, mySlice1, mySlice1, "", "", int32( c.Int("limit")), int32( c.Int("offset")), "")
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+          case "get-menu":
+          var menusApi swagger.MenusApi = *swagger.NewMenusApi()
+          
+          x,response,error := menusApi.GetAccountMenu(1315091, int32(c.Int("id")))
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+          case "list-queues":
+           var queuesApi swagger.QueuesApi = *swagger.NewQueuesApi()
+           mySlice1 := make([]string, 0)
+        x,response,error := queuesApi.ListAccountQueues(1315091, mySlice1, mySlice1, "", "", int32( c.Int("limit")), int32( c.Int("offset")), "")
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+           case "get-queue":
+         var queuesApi swagger.QueuesApi = *swagger.NewQueuesApi()
+          
+          x,response,error := queuesApi.GetAccountQueue(1315091, int32(c.Int("id")))
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+                    case "list-routes":
+           var routesApi swagger.RoutesApi = *swagger.NewRoutesApi()
+           mySlice1 := make([]string, 0)
+        x,response,error := routesApi.ListAccountRoutes(1315091, mySlice1, mySlice1, "", "", int32( c.Int("limit")), int32( c.Int("offset")), "")
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+           case "get-route":
+         var routesApi swagger.RoutesApi = *swagger.NewRoutesApi()
+          
+          x,response,error := routesApi.GetAccountRoute(1315091, int32(c.Int("id")))
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+           case "list-schedules":
+           var schedulesApi swagger.SchedulesApi = *swagger.NewSchedulesApi()
+           mySlice1 := make([]string, 0)
+        x,response,error := schedulesApi.ListAccountSchedules(1315091, mySlice1, mySlice1, "", "", int32( c.Int("limit")), int32( c.Int("offset")), "")
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+              case "get-schedule":
+        var schedulesApi swagger.SchedulesApi = *swagger.NewSchedulesApi()
+          
+          x,response,error := schedulesApi.GetAccountSchedule(1315091, int32(c.Int("id")))
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+           case "list-sms":
+           var smsApi swagger.SmsApi = *swagger.NewSmsApi()
+           mySlice1 := make([]string, 0)
+        x,response,error := smsApi.ListAccountSms(1315091, mySlice1, "","", "", "", int32( c.Int("limit")), int32( c.Int("offset")), "")
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+                case "get-sms":
+      var smsApi swagger.SmsApi = *swagger.NewSmsApi()
+          
+          x,response,error := smsApi.GetAccountSms(1315091, int32(c.Int("id")))
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+           case "list-available-phone-numbers":
+           var availableNumbersApi swagger.AvailablenumbersApi = *swagger.NewAvailablenumbersApi()
+           mySlice1 := make([]string, 0)
+        x,response,error := availableNumbersApi.ListAvailablePhoneNumbers(mySlice1, mySlice1,mySlice1,mySlice1,mySlice1,mySlice1,mySlice1,mySlice1,mySlice1,mySlice1,"", "", "", int32( c.Int("limit")), int32( c.Int("offset")), "")
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+              case "list-subaccounts":
+           var subaccountsApi swagger.SubaccountsApi = *swagger.NewSubaccountsApi()
+           mySlice1 := make([]string, 0)
+        x,response,error := subaccountsApi.ListAccountSubaccounts(1315091, mySlice1, mySlice1, "", "", int32( c.Int("limit")), int32( c.Int("offset")), "")
+        if error!=nil {
+            panic(error)
+            return nil
+            
+        }
+           _ = response
+      fmt.Printf("%+v\n",x)
+          
+          
+      default:
+          fmt.Println("Command not valid")
+      }
     return nil
   }
 
