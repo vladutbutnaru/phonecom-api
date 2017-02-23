5274a98002b72f54fb081a09cc85f4049fc583f2
Filip Ilioski <ilioskipilif@gmail.com>
2017-02-23 10:40:17 +0100

    extracted cli params into a variable

--- src/phonecom/phonecom.go
+++ src/phonecom/phonecom.go
@@ -12,55 +12,7 @@ func main() {
 
   app := cli.NewApp()
 
-  app.Flags = []cli.Flag{
-
-    cli.StringFlag{
-      Name: "command, c",
-      Value: listAccounts,
-      Usage: "Phone.com API command that you want to execute",
-    },
-    cli.IntFlag{
-      Name: "id",
-      Value: 0,
-      Usage: "ID of entity you want to operate",
-    },
-    cli.IntFlag{
-      Name: "id-secondary, is",
-      Value: 0,
-      Usage: "Secondary ID of entity you want to operate",
-    },
-    cli.IntFlag{
-      Name: "limit, l",
-      Value: 5,
-      Usage: "Upper limit of results you want to fetch",
-    },
-    cli.IntFlag{
-      Name: "offset, o",
-      Value: 0,
-      Usage: "Offset of results you want to fetch",
-    },
-    cli.IntFlag{
-      Name: "account, a",
-      Value: 1315091,
-      Usage: "Phone.com API account to use in API calls",
-    },
-    cli.StringFlag{
-      Name: "dryrun, d",
-      Value: "",
-      Usage: "Print the expected action without executing the API command",
-    },
-		cli.StringFlag{
-			Name: "input, i",
-			Value: "",
-			Usage: "Specify the path to the JSON file for making the API call",
-		},
-		cli.StringFlag{
-			Name: "verbose, vr",
-			Value: "",
-			Usage: "Activate verbose mode",
-		},
-
-  }
+  app.Flags = cliFlags
 
   app.Action = func(c *cli.Context) error {
     return execute(c)
