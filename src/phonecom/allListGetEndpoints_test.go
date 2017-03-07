5db5db7e8c6116d530b8651d409a31fc2a0e1b5e
Filip Ilioski <ilioskipilif@gmail.com>
2017-03-07 16:21:56 +0100

    Added multivalue filters

--- src/phonecom/allListGetEndpoints_test.go
+++ src/phonecom/allListGetEndpoints_test.go
@@ -268,3 +268,56 @@ func TestListAvailablePhoneNumberRegions(t *testing.T) {
   err, _ = createCli(listAvailablePhoneNumberRegions)
   assertErrorNotNull(t, err)
 }
+
+func TestListAvailablePhoneNumbers2(t *testing.T) {
+
+  var err error
+
+  phoneNumberSlice := make([]string, 0)
+  phoneNumberSlice = append(phoneNumberSlice, "+12234687")
+  countryCodeSlice := make([]string, 0)
+  countryCodeSlice = append(countryCodeSlice, "1")
+  npaSlice := make([]string, 0)
+  npaSlice = append(npaSlice, "gt:10")
+  nxxSlice := make([]string, 0)
+  nxxSlice = append(nxxSlice, "831")
+  xxxxSlice := make([]string, 0)
+  xxxxSlice = append(xxxxSlice, "7863")
+  citySlice := make([]string, 0)
+  citySlice = append(citySlice, "Martinsville")
+  provinceSlice := make([]string, 0)
+  provinceSlice = append(provinceSlice, "IN")
+  countrySlice := make([]string, 0)
+  countrySlice = append(countrySlice, "US")
+  priceSlice := make([]string, 0)
+  priceSlice = append(priceSlice, "0")
+  categorySlice := make([]string, 0)
+  categorySlice = append(categorySlice, "1")
+
+
+  err, _ = createCliListAvailablePhoneNumbers(listAvailablePhoneNumbers, phoneNumberSlice, countryCodeSlice, npaSlice, nxxSlice, xxxxSlice, citySlice, provinceSlice, countrySlice, priceSlice, categorySlice)
+  assertErrorNotNull(t, err)
+}
+
+func TestListAvailablePhoneNumberRegions2(t *testing.T) {
+
+  var err error
+
+  countryCodeSlice := make([]string, 0)
+  countryCodeSlice = append(countryCodeSlice, "1")
+  npaSlice := make([]string, 0)
+  npaSlice = append(npaSlice, "not-empty")
+  nxxSlice := make([]string, 0)
+  nxxSlice = append(nxxSlice, "249")
+  isTollFreeSlice := make([]string, 0)
+  isTollFreeSlice = append(isTollFreeSlice, "true")
+  citySlice := make([]string, 0)
+  citySlice = append(citySlice, "Prilep")
+  provincePostalCodeSlice := make([]string, 0)
+  provincePostalCodeSlice = append(provincePostalCodeSlice, "2345")
+  countryPostalCodeSlice := make([]string, 0)
+  countryPostalCodeSlice = append(countryPostalCodeSlice, "2345")
+
+  err, _ = createCliListAvailablePhoneNumberRegions(listAvailablePhoneNumberRegions, countryCodeSlice, npaSlice, nxxSlice, isTollFreeSlice, citySlice, provincePostalCodeSlice, countryPostalCodeSlice)
+  assertErrorNotNull(t, err)
+}
