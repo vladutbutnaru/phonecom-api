ad2bd49c2695743e13654f4a2368a0a534ab0667
Igor Simevski <i.simevski@gmail.com>
2017-03-12 22:41:47 +0100

    updated swagger-gen

--- swagger-gen.sh
+++ swagger-gen.sh
@@ -4,7 +4,18 @@ swaggerFilePath=phonecom.json
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:get -DremoteRepositories=central -Dartifact=io.swagger:swagger-codegen-cli:LATEST -Dpackaging=jar -Ddest=swagger-codegen.jar
 
-declare -a sdks=("android" "csharp" "go" "java" "objc" "php" "python" "ruby" "swift3" "typescript-node")
+declare -a sdks=(
+  "android"
+  "csharp"
+  "go"
+  "java"
+  "objc"
+  "php"
+  "python"
+  "ruby"
+  "swift3"
+  "typescript-node"
+)
 
 for sdk in "${sdks[@]}"
 do
