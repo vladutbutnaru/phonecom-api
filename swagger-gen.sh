f31b6aea0a7ba6c887624996e6da956c61d2f72f
Igor Simevski <i.simevski@gmail.com>
2017-03-12 22:32:49 +0100

    updated swagger-gen. removed unnecessary libs

--- swagger-gen.sh
+++ swagger-gen.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
+
+swaggerFilePath=phonecom.json
+
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:get -DremoteRepositories=central -Dartifact=io.swagger:swagger-codegen-cli:LATEST -Dpackaging=jar -Ddest=swagger-codegen.jar
-java -jar swagger-codegen.jar generate -i phonecom.json -l android -o SDKs/android-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l csharp -o SDKs/csharp-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l go -o SDKs/go-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l java -o SDKs/java-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l objc -o SDKs/objc-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l php -o SDKs/php-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l python -o SDKs/python-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l ruby -o SDKs/ruby-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l swift3 -o SDKs/swift3-client-generated
-java -jar swagger-codegen.jar generate -i phonecom.json -l typescript-node -o SDKs/typescript-node-client-generated
+
+declare -a sdks=("android" "csharp" "go" "java" "objc" "php" "python" "ruby" "swift3" "typescript-node")
+
+for sdk in "${sdks[@]}"
+do
+  java -jar swagger-codegen.jar generate -i $swaggerFilePath -l $sdk -o SDKs/$sdk-client-generated
+done
